ad53d438f59e994805d7a73b93b69bfa0ba8b573
Filip Knez <filip.knez@student.um.si>
2026-04-14 18:56:22 +0200

    tel

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'DONDOG_THEME_VERSION', '1.0.9' );
+define( 'DONDOG_THEME_VERSION', '1.0.10' );
 
 require_once __DIR__ . '/inc/theme-assets.php';
 require_once __DIR__ . '/inc/site-visibility.php';
