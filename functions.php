c163fb21206d77a28e5307463c832ab4867e670f
Filip Knez <filip.knez@student.um.si>
2026-05-04 23:53:05 +0200

    footer

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'DONDOG_THEME_VERSION', '1.0.65' );
+define( 'DONDOG_THEME_VERSION', '1.0.66' );
 
 require_once __DIR__ . '/inc/languages.php';
 require_once __DIR__ . '/inc/cookieyes-i18n.php';
